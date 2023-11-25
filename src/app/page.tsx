7a420aba837de3548d5a721bf3fb71b26214ff2f
Jure GS <jure@amber-it.co>
2023-11-25 17:39:32 +0100

    whatsapp test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 
 export default function Home() {
+  const text = encodeURIComponent(`Hello!`);
+  const phone = encodeURIComponent(`+385 91 161 4291`);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-8">
-      <div className="text-4xl">
-        Warp Code ltd
-      </div>
-      <div>
-        Website coming soon
-        <Link href={'/privacy-policy'}>PEEPEEPOOPOO</Link>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-8">
+      <div className="text-4xl">Warp Code ltd</div>
+      <a href="whatsapp://send?text=Bepis" className="text-4xl">
+        Test 1
+      </a>
+      <a href={`https://wa.me/${phone}?text=${text}`} className="text-4xl">
+        Test 2
+      </a>
     </main>
-  )
+  );
 }
